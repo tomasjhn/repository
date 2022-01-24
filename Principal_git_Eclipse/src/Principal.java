dc3533678ee4c77dec6e3c99abaa55a9c8a091a9
tomasjhn <97974371+tomasjhn@users.noreply.github.com>
2022-01-24 14:25:17 +0100

    Primer cambio en git en Eclipse

--- Principal_git_Eclipse/src/Principal.java
+++ Principal_git_Eclipse/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Primer cambio en git en Eclipse");
 	}
 
 }
