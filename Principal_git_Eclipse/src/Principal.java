50b11af669be8a6aacf32c3c47a0c6b47cc0d8fc
tomasjhn <97974371+tomasjhn@users.noreply.github.com>
2022-01-25 11:00:13 +0100

    Primer cambio en la rama en git en Eclipse

--- Principal_git_Eclipse/src/Principal.java
+++ Principal_git_Eclipse/src/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("Primer cambio en git en Eclipse");
+		System.out.println("Cambio en la rama en git en Eclipse");
 	}
 
 }
